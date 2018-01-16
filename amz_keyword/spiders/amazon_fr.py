4d343f0a28e35a9cff786ca333be37af11ba01e3
everAwayne@gmail.com <everAwayne@gmail.com>
2018-01-16 16:49:11 +0800

    update keyword

--- amz_keyword/spiders/amazon_fr.py
+++ amz_keyword/spiders/amazon_fr.py
@@ -6,12 +6,12 @@ class AMZFRSearchInfo(AMZSearchInfo):
     """
     def get_search_result(self):
         dct = {'count': None, 'category': []}
-        text_ls = self.soup.xpath('//div[@id="s-result-info-bar-content"]//h1[@id="s-result-count"]/text()')
+        text_ls = self.soup.xpath('//div[@id="s-result-info-bar-content"]//*[@id="s-result-count"]/text()')
         text = ' '.join(text_ls).lower()
         reg_ret = re.search(r'([\d\s]+)\s*résultats', text)
         if reg_ret:
             dct['count'] = int(reg_ret.group(1).replace('\xa0',''))
-        text_ls = self.soup.xpath('//div[@id="s-result-info-bar-content"]//h1[@id="s-result-count"]/span/a/text()')
+        text_ls = self.soup.xpath('//div[@id="s-result-info-bar-content"]//*[@id="s-result-count"]/span/a/text()')
         if text_ls:
             dct['category'] = [item.strip() for item in text_ls]
         return dct
